76fe9ee3330a1db7f055be6829b5bea37587f8c8
Алексей <32929527+nub24@users.noreply.github.com>
2023-04-19 13:36:34 +0700

    lastPR10

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,13 +1,13 @@
-import { address, token } from './constants';
+import { address, token } from "./constants";
 
 class Api {
-  constructor ({ address, token }) {
+  constructor({ address, token }) {
     this._address = address;
     this._token = token;
     this._headers = {
       authorization: this._token,
-      "Content-Type": "application/json"
-    }
+      "Content-Type": "application/json",
+    };
   }
 
   //обработка запроса
@@ -15,54 +15,50 @@ class Api {
     if (res.ok) {
       return res.json();
     } else {
-      Promise.reject(`Ошибка ${res.status}`)
+      Promise.reject(`Ошибка ${res.status}`);
     }
   };
 
   //получение информации о пользователе с сервера
-  getUserInfo () {
+  getUserInfo() {
     return fetch(`${this._address}/users/me`, {
-      headers: this._headers
+      headers: this._headers,
     }).then(this._handleResponse);
   }
 
   //получение карточек
-  getCards () {
+  getCards() {
     return fetch(`${this._address}/cards`, {
-      headers: this._headers
-    })
-    .then(this._handleResponse);
+      headers: this._headers,
+    }).then(this._handleResponse);
   }
 
   //передача карточки на сервер
-  postCard ({ name, link }) {
+  postCard({ name, link }) {
     return fetch(`${this._address}/cards`, {
-      method: 'POST',
+      method: "POST",
       headers: this._headers,
-      body: JSON.stringify ({
+      body: JSON.stringify({
         name,
-        link
-      })
-    })
-    .then(this._handleResponse);
+        link,
+      }),
+    }).then(this._handleResponse);
   }
 
   //простановка лайка
-  _putLike (id) {
+  _putLike(id) {
     return fetch(`${this._address}/cards/${id}/likes`, {
-      method: 'PUT',
-      headers: this._headers
-    })
-    .then(this._handleResponse);
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._handleResponse);
   }
 
   //удоление лайка
-  _delLike (id) {
+  _delLike(id) {
     return fetch(`${this._address}/cards/${id}/likes`, {
-      method: 'DELETE',
-      headers: this._headers
-    })
-    .then(this._handleResponse);
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._handleResponse);
   }
 
   //установка/снятие лайка
@@ -79,22 +75,21 @@ class Api {
   }
 
   //передача информации о пользователе на сервер
-  patchProfile ( {name, about} ) {
+  patchProfile({ name, about }) {
     return fetch(`${this._address}/users/me`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: this._headers,
-      body: JSON.stringify({ name, about })
-    }).then(this._handleResponse)
+      body: JSON.stringify({ name, about }),
+    }).then(this._handleResponse);
   }
 
   //передача аватарки на сервер
   patchAvatar(avatar) {
     return fetch(`${this._address}/users/me/avatar`, {
       method: "PATCH",
-      body: JSON.stringify({avatar}),
-      headers: this._headers
-    })
-    .then(this._handleResponse);
+      body: JSON.stringify({ avatar }),
+      headers: this._headers,
+    }).then(this._handleResponse);
   }
 }
 
